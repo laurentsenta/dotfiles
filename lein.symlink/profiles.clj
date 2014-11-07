2d61475c37d0e8059768aca13198d54878f34583
lsenta <laurent.senta@gmail.com>
2014-11-07 16:54:16 +0000

    Add lein namespace checker and java8
    Signed-off-by: lsenta <laurent.senta@gmail.com>

--- lein.symlink/profiles.clj
+++ lein.symlink/profiles.clj
@@ -1,6 +1,14 @@
-{:user {:plugins [
-                  [lein-ancient "0.5.5"]
-                  [lein-kibit "0.0.8"]
-                  [jonase/eastwood "0.1.4"]
-                  [lein-bikeshed "0.1.8"]
-                 ]}}
+{:user {
+         :plugins [
+                    [lein-midje "3.1.3"]
+                    [lein-ancient "0.5.5"]
+                    [lein-kibit "0.0.8"]
+                    [jonase/eastwood "0.1.4"]
+                    [lein-bikeshed "0.1.8"]
+                    [slamhound "1.5.5"]
+                    ]
+         :aliases {
+                    "slamhound" ["run" "-m" "slam.hound"]
+                  }}}
+
+

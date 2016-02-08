b04c12df8e4c95386c16896fe3a33c8d367892bb
lsenta <laurent.senta@gmail.com>
2016-02-08 17:21:41 +0100

    add platform & merge lein
    Signed-off-by: lsenta <laurent.senta@gmail.com>

--- lein.symlink/profiles.clj
+++ lein.symlink/profiles.clj
@@ -3,6 +3,7 @@
                         [cljfmt "0.1.10"]
                         [jonase/eastwood "0.2.1" :exclusions [org.clojure/clojure]]
                         ]
+        :test-refresh {:notify-command ["terminal-notifier" "-title" "Tests" "-message"]}
          :plugins [
                     [lein-midje "3.1.3"]
                     [lein-ancient "0.5.5"]
@@ -10,9 +11,12 @@
                     [jonase/eastwood "0.2.1" :exclusions [org.clojure/clojure]]
                     [lein-bikeshed "0.1.8"]
                     [slamhound "1.5.5"]
+                    [lein-ubersource "0.1.1"]
+                    [com.jakemccrary/lein-test-refresh "0.11.0"]
                     ]
+        :android {
+                  :sdk-path "/Users/lsenta/dev/android-sdk-macosx"
+                  }
          :aliases {
                     "slamhound" ["run" "-m" "slam.hound"]
                   }}}
-
-

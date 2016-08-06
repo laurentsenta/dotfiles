e58f2a3b5f0ad73911a60a5b8e183366232cbd36
lsenta <laurent.senta@gmail.com>
2016-08-06 17:25:04 +0200

    try provide setup script for ubuntu
    Signed-off-by: lsenta <laurent.senta@gmail.com>

---
+++ setup_ubuntu1604.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+
+sudo apt-get install vim git
+
+if [ "`whoami`" == "root" ]; then
+    echo "set root password:"
+    passwd
+    echo "creating my user:"
+    adduser laurent
+    usermod -aG sudo laurent
+    echo "in the next, update hostname"
+    vim /etc/hostname
+    vim /etc/hosts
+    echo "moving to regular user"
+    su laurent
+fi
+
+cd ~
+mkdir .ssh/
+echo "Enter your ssh public key"
+cat >> .ssh/authorized_keys
+echo "remove password login from ssh config (PasswordAuthentication)"
+vim /etc/ssh/sshd_config
+
+ssh-keygen -t rsa -b 4096 -C 'laurent.senta@gmail.com'
+echo "go to https://github.com/settings/keys and add your new ssh key:"
+cat ~/.ssh/id_rsa.pub
+
+mkdir ~/dev
+cd ~/dev
+git clone 'git@github.com:lsenta/dotfiles.git'
+cd dotfiles
+
+./script/1_bootstrap
+echo "setup vim, skip the errors then call :BundleInstall"
+vim
+
+./script/2_init
+./script/3_install
+
+echo "server? run ./script/4_server"
+
+
+
+
+
+
+
+
+
+

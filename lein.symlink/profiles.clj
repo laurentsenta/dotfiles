b84a2d2a14281197309fe2d0c3c2503753e8ebf5
lsenta <laurent.senta@gmail.com>
2016-09-19 14:01:05 +0200

    update clojure + add spacemacs
    Signed-off-by: lsenta <laurent.senta@gmail.com>

--- lein.symlink/profiles.clj
+++ lein.symlink/profiles.clj
@@ -1,16 +1,23 @@
-{:user {
+ {
+  :repl {:plugins [[cider/cider-nrepl "0.10.0-SNAPSHOT"]
+                   [refactor-nrepl "2.0.0-SNAPSHOT"]]
+         :dependencies [[alembic "0.3.2"]
+                        [org.clojure/tools.nrepl "0.2.12"]]}
+  :user {
         :dependencies [
                        [cljfmt "0.1.10"]
                        [jonase/eastwood "0.2.1" :exclusions [org.clojure/clojure]]
                        ]
         :test-refresh {:notify-command ["reattach-to-user-namespace" "terminal-notifier" "-title" "Tests" "-message"]}
         :plugins [
+                  ;; 14 aug 2016
+                  [lein-ancient "0.6.10"]
+                  [lein-bikeshed "0.3.0"]
+                  [lein-kibit "0.1.2"]
+                  [jonase/eastwood "0.2.3" :exclusions [org.clojure/clojure]]
+                  ;; older
                   [lein-pprint "1.1.1"]
                   [lein-midje "3.1.3"]
-                  [lein-ancient "0.5.5"]
-                  [lein-kibit "0.0.8"]
-                  [jonase/eastwood "0.2.1" :exclusions [org.clojure/clojure]]
-                  [lein-bikeshed "0.1.8"]
                   [slamhound "1.5.5"]
                   [lein-ubersource "0.1.1"]
                   [com.jakemccrary/lein-test-refresh "0.14.0"]

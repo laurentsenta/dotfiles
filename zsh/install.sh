e458e9864ac2ff23086f6eeef03d1922740f923b
Laurent Senta <laurent@singulargarden.com>
2022-04-04 10:35:21 +0200

    add docker, dockercompose completion, sed and k8s

--- zsh/install.sh
+++ zsh/install.sh
@@ -4,3 +4,8 @@ brew install zsh autojump ack zsh-syntax-highlighting zsh-autosuggestions fd tre
 $(brew --prefix)/opt/fzf/install
 
 chsh -s $(which zsh)
+
+echo "remember to update your completion scripts"
+
+echo "initially: https://raw.githubusercontent.com/docker/cli/a4b6fe16ae7fe9260ee1960be6da2e050e493e98/contrib/completion/zsh/_docker"
+echo "initially: https://raw.githubusercontent.com/docker/compose/1.29.2/contrib/completion/zsh/_docker-compose"
